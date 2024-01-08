06d2bc1513cff129f7794c825b621b5def2dd9f3
shankar55 <shankar511998@gmail.com>
2024-01-08 11:35:56 +0530

    Updates

---
+++ precache-manifest.f0380a3be9b18cec2baf4b864708a4b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6bfc17cfd2cd7ffb0dda0fd0ca89a5",
+    "url": "/ui-contact-list/index.html"
+  },
+  {
+    "revision": "56bf07d2386578ac069e",
+    "url": "/ui-contact-list/static/js/2.afccd033.chunk.js"
+  },
+  {
+    "revision": "0d1152ac1fed7e0f171b",
+    "url": "/ui-contact-list/static/js/main.cff7f782.chunk.js"
+  },
+  {
+    "revision": "7626e1c8dda7a23ed162",
+    "url": "/ui-contact-list/static/js/runtime-main.6cfd0b83.js"
+  }
+]);
